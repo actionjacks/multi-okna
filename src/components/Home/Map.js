40a1808e816691d6d4217e7426ddd2d82a653fb3
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-05-25 14:44:21 +0200

    handle marker click map component

--- src/components/Home/Map.js
+++ src/components/Home/Map.js
@@ -1,5 +1,10 @@
-import React from "react";
-import { GoogleMap, useJsApiLoader, InfoWindow } from "@react-google-maps/api";
+import React, { useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  InfoWindow,
+  Marker,
+} from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
@@ -18,12 +23,14 @@ function MyComponent() {
     googleMapsApiKey: "AIzaSyA1alshfkMwsP8leA9dRSxVuaplwBZigCA",
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = useState(null);
+  const [show, setShow] = useState(true);
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = React.useCallback((map) => {
     const bounds = new window.google.maps.LatLngBounds();
     // map.fitBounds(bounds);
     setMap(map);
+    console.log(map);
   }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
@@ -51,15 +58,23 @@ function MyComponent() {
       >
         {/* Child components, such as markers, info windows, etc. */}
         <>
-          <InfoWindow onLoad={onLoad} position={centers[0]}>
-            <div style={divStyle}>
-              <h2>Multi Okna sp. z o.o.</h2>
-              <p>Tu jesteśmy</p>
-              <a style={linkStyle} href="tel:607473123">
-                Tel 607 473 123
-              </a>
-            </div>
-          </InfoWindow>
+          <Marker
+            onLoad={onLoad}
+            position={centers[0]}
+            onClick={() => setShow(!show)}
+          >
+            {show && (
+              <InfoWindow onLoad={onLoad} position={centers[0]}>
+                <div style={divStyle}>
+                  <h2>Multi Okna sp. z o.o.</h2>
+                  <p>Tu jesteśmy</p>
+                  <a style={linkStyle} href="tel:607473123">
+                    Tel 607 473 123
+                  </a>
+                </div>
+              </InfoWindow>
+            )}
+          </Marker>
         </>
       </GoogleMap>
     </div>
