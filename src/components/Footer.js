38cdc5cafed4e9485820b9ddae18d9e9495b33bd
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-05-27 15:06:28 +0200

    footer styles scroll top handle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import scroll from "react-scroll";
 
 import { withStyles } from "@material-ui/core/styles";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import YouTubeIcon from "@material-ui/icons/YouTube";
+import ForwardIcon from "@material-ui/icons/Forward";
 
 import styles from "../styles/FooterStyle";
 
 function Footer({ classes }) {
+  const handleClick = () => {
+    scroll.animateScroll.scrollToTop();
+  };
+
   return (
     <div className={classes.root}>
-      <div>
-        <FacebookIcon />
-        <InstagramIcon />
-        <YouTubeIcon />
+      <div className={classes.iconContainer}>
+        <Link>
+          <FacebookIcon className={classes.icon} />
+        </Link>
+        <Link>
+          <InstagramIcon className={classes.icon} />
+        </Link>
+        <Link>
+          <YouTubeIcon className={classes.icon} />
+        </Link>
       </div>
       <div className={classes.contactContainer}>
-        <a href="tel:607473123">zadzwoń 607 473 123</a>
-        <a href="mailto:biuro@multi-okna.pl">biuro@multi-okna.pl</a>
+        <a className={classes.footerLink} href="tel:607473123">
+          zadzwoń 607 473 123
+        </a>
+        <a className={classes.footerLink} href="mailto:biuro@multi-okna.pl">
+          biuro@multi-okna.pl
+        </a>
       </div>
+      <ForwardIcon onClick={handleClick} className={classes.upIcon} />
+      <span className={classes.credits}>
+        Projekt i wykonanie: actionjacks22@gmail.com
+      </span>
     </div>
   );
 }
